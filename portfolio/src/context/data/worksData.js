dcf55c44a790aacb9f81060e5263b09e33f4c1c9
zempo <zelenkosolomon@gmail.com>
2021-05-06 10:00:44 -0700

    add data

---
+++ portfolio/src/context/data/worksData.js
@@ -0,0 +1,13 @@
+export const projects = [
+  {
+    title: `Above the Line`,
+    // todo: add imgur screenshot(s)
+    screenshot: ``,
+    live_site: `https://atl-client.vercel.app/`,
+    github_repo: `https://github.com/zempo/atl-client`,
+    desc: `Above the Line is your personalized scriptwriting environment. Craft scripts and screenplays - as fast as you can think!`,
+    type: `solo`,
+    tags: [`pern`, `fullstack`],
+    recently_updated: `04-28-2021`,
+  },
+];
---
+++ portfolio/src/context/state/WorksState.js
@@ -0,0 +1,16 @@
+import React, { useReducer } from "react";
+
+const WorksState = (props) => {
+  const initialState = {
+    works: [],
+    searchWorks: [],
+  };
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default WorksState;
