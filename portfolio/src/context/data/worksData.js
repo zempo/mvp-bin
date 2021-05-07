5044c2799fbe82180c42c40de77715634be3d8ea
zempo <zelenkosolomon@gmail.com>
2021-05-07 13:54:21 -0700

    update project data

--- portfolio/src/context/data/worksData.js
+++ portfolio/src/context/data/worksData.js
@@ -12,6 +12,11 @@ export const projects = [
     type: `solo`,
     desc: `Above the Line is your personalized scriptwriting environment. Craft scripts and screenplays - as fast as you can think!`,
     live_site: `https://above-the-line.vercel.app`,
+    // add guest creds to ATL
+    guest_creds: {
+      email: "guest@solomonzelenko.dev",
+      password: "ForHire1234@!",
+    },
     github_repo: `https://github.com/zempo/atl-client`,
     preview_img: ``,
     screenshots: [``],
@@ -20,9 +25,13 @@ export const projects = [
     title: `Just the Occasion`,
     tags: [`pern`, `fullstack`],
     type: `solo`,
-    desc: `Above the Line is your personalized scriptwriting environment. Craft scripts and screenplays - as fast as you can think!`,
-    live_site: `https://above-the-line.vercel.app`,
-    github_repo: `https://github.com/zempo/atl-client`,
+    desc: `Just the Occasion is the greeting card platform. Customize, share, and download greeting cards on your terms. Discover a platform that values your experiences.`,
+    live_site: `https://just-the-occasion.com/`,
+    guest_creds: {
+      email: "guest@solomonzelenko.dev",
+      password: "ForHire1234@!",
+    },
+    github_repo: `https://github.com/zempo/jto-client`,
     preview_img: ``,
     screenshots: [``],
   },
