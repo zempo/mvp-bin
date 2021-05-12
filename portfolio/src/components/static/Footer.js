e7fca2d050c837ba83f4be271a1fe8807865be82
zempo <zelenkosolomon@gmail.com>
2021-05-11 23:09:01 -0700

    reword footer

--- portfolio/src/components/static/Footer.js
+++ portfolio/src/components/static/Footer.js
@@ -5,7 +5,7 @@ import "../../styles/Static.scss";
 export const Footer = () => {
   return (
     <footer className='app-footer'>
-      <h3>Code Your Vision&trade;</h3>
+      <h3>Let's code your vision.</h3>
       <p>&copy; {returnYear()} Solomon Zelenko.</p>
       <p>All rights reserved. Powered by React.</p>
     </footer>
