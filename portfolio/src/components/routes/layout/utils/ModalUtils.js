ffbfded7c203b36e35c102eaa316e675b5b758cf
zempo <zelenkosolomon@gmail.com>
2021-06-17 18:34:37 -0700

    edit byte appearance

--- portfolio/src/components/routes/layout/utils/ModalUtils.js
+++ portfolio/src/components/routes/layout/utils/ModalUtils.js
@@ -223,20 +223,11 @@ export const WorkModal = ({ item, modalType, hide }) => {
 export const ByteModal = ({ item, modalType, hide }) => {
   const BytesContext = useContext(bytesContext);
   const { currentByte } = BytesContext;
-  const {
-    id,
-    title,
-    type,
-    desc,
-    link,
-    github_repo,
-    guest_creds,
-    preview_img,
-    code_embed,
-  } = currentByte[0];
+  const { id, title, type, desc, link, github_repo, preview_img, code_embed } =
+    currentByte[0];
 
   return (
-    <div className={`modal-pg ${modalType}-modal`}>
+    <div className={`modal-pg ${modalType}-modal`} id={id}>
       <ModalControls hide={hide} />
       <header>
         <h1>{title}</h1>
@@ -258,13 +249,44 @@ export const ByteModal = ({ item, modalType, hide }) => {
           ) : null}
         </div>
         {code_embed ? (
-          <ReactEmbedGist gist={getGist(github_repo)} />
+          <>
+            <div className='modal-desc'>
+              <p>
+                {splitParas(desc).map((s, i) =>
+                  s.includes("NOTE") ? (
+                    <span key={i} className='note'>
+                      *{s}*
+                    </span>
+                  ) : (
+                    <span key={i}>{s} </span>
+                  )
+                )}
+              </p>
+            </div>
+            <ReactEmbedGist gist={getGist(github_repo)} />
+          </>
         ) : (
-          <img
-            className='modal-preview'
-            src={preview_img}
-            alt={`Preview for ${type} named, "${title}".`}
-          />
+          <>
+            <img
+              className='modal-preview'
+              src={preview_img}
+              alt={`Preview for ${type} named, "${title}".`}
+            />
+            <div className='modal-desc'>
+              <h3>{capitalizeStr(type)} Description</h3>
+              <p>
+                {splitParas(desc).map((s, i) =>
+                  s.includes("NOTE") ? (
+                    <span key={i} className='note'>
+                      *{s}*
+                    </span>
+                  ) : (
+                    <span key={i}>{s} </span>
+                  )
+                )}
+              </p>
+            </div>
+          </>
         )}
       </div>
     </div>
