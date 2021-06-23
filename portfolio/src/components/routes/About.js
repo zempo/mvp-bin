10040c687ee964e38968d2217a4d06b599b36666
zempo <zelenkosolomon@gmail.com>
2021-06-23 15:52:19 -0700

    update about page

--- portfolio/src/components/routes/About.js
+++ portfolio/src/components/routes/About.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { CountUpAnimation } from "./layout/utils/AnimationUtils";
 import { fetchRepos } from "../../services/endpointsService";
 import { gitApi } from "../../config";
 import "../../styles/pages/About.scss";
 
 const initialStats = {
+  avatar_url: "https://avatars.githubusercontent.com/u/39205476?v=4",
+  repos: false,
+  contributions: false,
+  followers: false,
+};
+
+const fallbackStats = {
+  avatar_url: "https://avatars.githubusercontent.com/u/39205476?v=4",
   repos: 71,
+  contributions: 1000,
+  followers: 25,
 };
 
 export const About = () => {
@@ -14,17 +25,29 @@ export const About = () => {
     document.title = "About";
 
     const getRepoData = async () => {
+      const API = `client_id=${gitApi.CLIENT_ID}&client_secret=${gitApi.CLIENT_SECRET}`;
+
       try {
-        const resRepos = await fetchRepos(
-          `/users/zempo/repos?per_page=200&client_id=${gitApi.CLIENT_ID}&client_secret=${gitApi.CLIENT_SECRET}`
-        );
-        console.log(resRepos.data.length);
+        const resStats = await fetchRepos(`/users/zempo?${API}`);
+        // const resContributions = await fetchRepos(
+        //   `/users/zempo/events/public?per_page=100&${API}`
+        // );
+
+        console.log(resStats.data);
+
+        setStats({
+          avatar_url: resStats.data.avatar_url,
+          repos: resStats.data.public_repos,
+          contributions: 700,
+          followers: resStats.data.followers,
+        });
       } catch (err) {
         console.log(err.response);
+        setStats(fallbackStats);
       }
     };
 
-    // getRepoData();
+    getRepoData();
     // eslint-disable-next-line
   }, []);
 
@@ -37,22 +60,54 @@ export const About = () => {
           living in <span>LA</span>.
         </h2>
       </header>
-      {/* <div className='about-stats'>
-        <div className='stat-repo'>
-          <h3>{stats.repos}</h3>
-          <p>Repos</p>
+      <hr />
+      <div className='about-stats'>
+        <div className='stat stat-repo'>
+          <h3>
+            {stats.repos ? (
+              <CountUpAnimation>{stats.repos}</CountUpAnimation>
+            ) : (
+              0
+            )}
+          </h3>
+          <p>
+            Public <br /> Repositories
+          </p>
         </div>
-        <div>
-          <h3>{stats.repos}</h3>
-          <p>Contributions</p>
+        <div className='stat stat-contributions'>
+          <h3>
+            {stats.contributions ? (
+              <>
+                <CountUpAnimation>{stats.contributions}</CountUpAnimation>+
+              </>
+            ) : (
+              0
+            )}
+          </h3>
+          <p>
+            GitHub <br /> Contributions
+          </p>
         </div>
-        <div>
-          <h3>{stats.repos}</h3>
-          <p>Followers</p>
+        <div className='stat stat-followers'>
+          <h3>
+            {stats.followers ? (
+              <CountUpAnimation>{stats.followers}</CountUpAnimation>
+            ) : (
+              0
+            )}
+          </h3>
+          <p>
+            GitHub <br /> Followers
+          </p>
         </div>
-      </div> */}
+      </div>
       <article className='about-summary'>
-        <h3>Hi there! 👋</h3>
+        <img
+          className='prof-img'
+          src={stats.avatar_url}
+          alt='Avatar for Solomon Zelenko'
+        />
+        <h3>Hi, there! 👋</h3>
         <p>
           I am a tech enthusiast and generalist living in
           <span> Los Angeles, CA</span>. Today, I work with&nbsp;
@@ -80,7 +135,31 @@ export const About = () => {
           <span> JavaScript &amp;&amp; Frameworks </span> to build 22
           <sup>nd</sup> century apps.
         </p>
-        <p></p>
+        <p>
+          Previously, I attended the <span>University of Denver </span>
+          where I graduated with a B.S. in Psychology. However, I soon found
+          myself at a crossroads. To support my education, I had worked with the
+          now defunct&nbsp;
+          <a
+            href='https://www.facebook.com/OpenWorldLearning/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <span>OpenWorld Learning (OWL)</span>
+          </a>
+          . During my time with OWL, I studied the{" "}
+          <span>
+            MicroWorlds Curriculum &amp;&amp; Constructivist Learning Theory{" "}
+          </span>
+          to help mentor elementary school students all across the{" "}
+          <span>Denver Front Range</span>. Shortly after graduating, I followed
+          my newfound passion and moved to California to continue learning how
+          to code.
+        </p>
+        <p>
+          Outside of the tech world, I am an avid adventurer, baker, and
+          voiceover.
+        </p>
       </article>
     </section>
   );
---
+++ portfolio/src/components/routes/layout/utils/AnimationUtils.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const easeOutQuad = (t) => t * (2 - t);
+const frameDuration = 1000 / 60;
+
+export const CountUpAnimation = ({ children, duration = 2000 }) => {
+  const countTo = parseInt(children, 10);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let frame = 0;
+    const totalFrames = Math.round(duration / frameDuration);
+    const counter = setInterval(() => {
+      frame++;
+      const progress = easeOutQuad(frame / totalFrames);
+      setCount(countTo * progress);
+
+      if (frame === totalFrames) {
+        clearInterval(counter);
+      }
+    }, frameDuration);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return Math.floor(count);
+};
